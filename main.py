090de8df303fc6dd2572cf9644f914ec3f6c6c62
Matt Boubin <mjb9353@nyu.edu>
2024-07-06 17:05:31 -0400

    simple object connection

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import numpy
-import pandas
+# -*- coding: utf-8 -*-
 
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+
+st = StanfordNERTagger('/standord-corenlp-4.5.7/stanford-corenlp-4.5.7-models-english',
+					   encoding='utf-8')
+
+text = 'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.'
+
+tokenized_text = word_tokenize(text)
+classified_text = st.tag(tokenized_text)
+
+print(classified_text)
