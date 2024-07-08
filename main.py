2f9089c9d005e9c9d945f035ef6d0c23efbe7ba5
Matthew J Boubin <mjb9353@ad.nyu.edu>
2024-07-08 14:27:43 -0400

    Added sf login

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import nltk
 from nltk.tokenize import word_tokenize
-
 nltk.download('punkt')
+
+def salesforce_login():
+    from credentials_connection import User
+    conn = User('dev')
+    conn.getCredentials()
+    sf = conn.sf_login()
+    return sf
+
+
+sf = salesforce_login()
+
 text = "Hello, how are you doing?"
 tokens = word_tokenize(text)
 print(tokens)
