e3d25ae296963a9ce8f4c36204a8fc4e2dc51bda
mjb9353 <mjb9353@ad.nyu.edu@WAG4028-LW.wagner.nyu.edu>
2024-08-05 11:25:35 -0400

    added output.txt to be used as input for RAG LLM

--- convert.py
+++ convert.py
@@ -3,11 +3,37 @@ import numpy as np
 import pandas as pd
 import csv
 
-dat = pd.read_csv('responses_anon.csv').iloc[0:5].to_string()
+dat = pd.read_csv('responses_anon.csv')
+# .iloc[0:50].to_string()
+text_file = open("Output.txt", "w")
+text_file.close()
+
+i = 0
+while(i< 2):
+    
+  text_file = open("Output.txt", "a")
+  url = "https://api.tableconvert.com/v2/convert/csv-to-textile"
+  payload = {
+    'data': dat.iloc[i*50:(i+1)*50].to_string(),
+    # 'file': ('f', open('responses_anon.csv', 'rb')),
+    # 'url': 'https://github.com/boubinmj/anonymizedNames/blob/main/responses_anon.csv',
+    'output[escape]': 'true',
+    'output[rowHeader]': 'true',
+    'output[thead]': 'true',
+  }
+  headers = {
+    # Get your API key: https://tableconvert.com/profile/
+    'Authorization': 'Bearer 2dl6AXWry1ZTYDTUR8NI4Ed9Icrkvpmt'
+  }
+
+  i = i + 1
+
+  response = requests.post(url, data=payload, headers=headers)
+  text_file.write(response.text)
 
 url = "https://api.tableconvert.com/v2/convert/csv-to-textile"
 payload = {
-  'data': dat,
+  'data': dat.iloc[i*50:len(dat)].to_string(),
   # 'file': ('f', open('responses_anon.csv', 'rb')),
   # 'url': 'https://github.com/boubinmj/anonymizedNames/blob/main/responses_anon.csv',
   'output[escape]': 'true',
@@ -16,10 +42,10 @@ payload = {
 }
 headers = {
   # Get your API key: https://tableconvert.com/profile/
-  'Authorization': 'Bearer <bearer>'
+  'Authorization': 'Bearer 2dl6AXWry1ZTYDTUR8NI4Ed9Icrkvpmt'
 }
 
 response = requests.post(url, data=payload, headers=headers)
-text_file = open("Output.txt", "w")
 text_file.write(response.text)
+
 text_file.close()
