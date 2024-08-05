0b5415c29317d9a51d7b82dc755e86536048ad10
Matt Boubin <mjb9353@nyu.edu>
2024-08-04 21:42:02 -0400

    created text file table with TableConvert API

---
+++ convert.py
@@ -0,0 +1,25 @@
+import requests
+import numpy as np
+import pandas as pd
+import csv
+
+dat = pd.read_csv('responses_anon.csv').iloc[0:5].to_string()
+
+url = "https://api.tableconvert.com/v2/convert/csv-to-textile"
+payload = {
+  'data': dat,
+  # 'file': ('f', open('responses_anon.csv', 'rb')),
+  # 'url': 'https://github.com/boubinmj/anonymizedNames/blob/main/responses_anon.csv',
+  'output[escape]': 'true',
+  'output[rowHeader]': 'true',
+  'output[thead]': 'true',
+}
+headers = {
+  # Get your API key: https://tableconvert.com/profile/
+  'Authorization': 'Bearer 2dl6AXWry1ZTYDTUR8NI4Ed9Icrkvpmt'
+}
+
+response = requests.post(url, data=payload, headers=headers)
+text_file = open("Output.txt", "w")
+text_file.write(response.text)
+text_file.close()
